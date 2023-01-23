61633f73642fb462d2d2a32c0e77fd232eca8a78
Jaehyun Ahn <desktop3711@g.hongik.ac.kr>
2023-01-23 21:34:25 +0900

    PostRepository 테스트 추가

---
+++ src/test/java/com/uwoobeat/book/domain/post/PostRepositoryTest.java
@@ -0,0 +1,45 @@
+package com.uwoobeat.book.domain.post;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class PostRepositoryTest {
+    @Autowired
+    PostRepository postRepository;
+
+    @AfterEach // @After in JUnit4
+    public void cleanup() {
+        postRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기() {
+        //given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postRepository.save(Post.builder()
+                .title(title)
+                .content(content)
+                .author("testauthor@gmail.com")
+                .build());
+
+        //when
+        List<Post> postList = postRepository.findAll();
+
+        //then
+        Post post = postList.get(0);
+        assertThat(post.getTitle()).isEqualTo(title);
+        assertThat(post.getContent()).isEqualTo(content);
+    }
+}
