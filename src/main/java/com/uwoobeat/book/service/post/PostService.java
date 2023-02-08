1925d4500164580820f9a712045b08b77ff14761
uwoobeat <desktop3711@g.hongik.ac.kr>
2023-02-08 18:45:26 +0900

    PostService 추가

---
+++ src/main/java/com/uwoobeat/book/service/post/PostService.java
@@ -0,0 +1,19 @@
+package com.uwoobeat.book.service.post;
+
+import com.uwoobeat.book.domain.post.PostRepository;
+import com.uwoobeat.book.web.dto.PostSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostService {
+    private final PostRepository postRepository;
+
+    @Transactional
+    public Long save(PostSaveRequestDto requestDto) {
+        return postRepository.save(requestDto.toEntity()).getId();
+    }
+}
+
