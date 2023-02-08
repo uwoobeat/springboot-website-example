e5653281312eec446ba72ec2c82b40c64c2c5375
uwoobeat <desktop3711@g.hongik.ac.kr>
2023-02-08 18:47:31 +0900

    PostSaveRequestDto 추가

---
+++ src/main/java/com/uwoobeat/book/web/dto/PostSaveRequestDto.java
@@ -0,0 +1,29 @@
+package com.uwoobeat.book.web.dto;
+
+import com.uwoobeat.book.domain.post.Post;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PostSaveRequestDto {
+    private String title;
+    private String content;
+    private String author;
+
+    @Builder
+    public PostSaveRequestDto(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    public Post toEntity() {
+        return Post.builder()
+                .title(title)
+                .content(content)
+                .author(author)
+                .build();
+    }
+}
