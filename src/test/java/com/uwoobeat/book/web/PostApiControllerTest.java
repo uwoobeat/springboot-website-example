1de63b786d58bfceeff6941c0b8a3d516769085c
uwoobeat <desktop3711@g.hongik.ac.kr>
2023-02-08 18:48:05 +0900

    PostApiController 테스트 추가

---
+++ src/test/java/com/uwoobeat/book/web/PostApiControllerTest.java
@@ -0,0 +1,63 @@
+package com.uwoobeat.book.web;
+
+import com.uwoobeat.book.domain.post.Post;
+import com.uwoobeat.book.domain.post.PostRepository;
+import com.uwoobeat.book.web.dto.PostSaveRequestDto;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class PostApiControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @AfterEach
+    public void tearDown() throws Exception {
+        postRepository.deleteAll();
+    }
+
+    @Test
+    public void Post_등록된다() throws Exception {
+        // given
+        String title = "title";
+        String content = "content";
+        PostSaveRequestDto requestDto = PostSaveRequestDto.builder()
+                .title(title)
+                .content(content)
+                .author("author")
+                .build();
+
+        String url = "http://localhost:" + port + "/api/v1/posts";
+
+        // when
+        ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url, requestDto, Long.class);
+
+        // then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+        List<Post> all = postRepository.findAll();
+        assertThat(all.get(0).getTitle()).isEqualTo(title);
+        assertThat(all.get(0).getContent()).isEqualTo(content);
+    }
+}
